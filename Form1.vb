af8e908c62f282020c33bc111cf1248f53150d0e
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-05 07:06:27 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.ComponentModel.Design.Serialization
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+        Dim originalphase As String
+        Dim embeddedphrase As String
+        Dim TxtExtract
+
+        n = 1
+        originalphase = " visual basic 2022 "
+        embeddedphrase = "basic"
+
+        TxtExtract = MsgBox(InStr(1, “Visual Basic”, “Basic”))
+    End Sub
+End Class
+
+
+
